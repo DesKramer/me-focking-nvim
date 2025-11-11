2996ce7dd3c26f88f9c6c2a00850f77d2c648a91
DesKramer <destkramers@gmail.com>
2025-11-11 16:33:46 +0000

    Some lililil updates

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -1,197 +1,208 @@
 -- Editor enhancement plugins
 
 return {
-  -- File explorer
-  {
-    "nvim-tree/nvim-tree.lua",
-    dependencies = { "nvim-tree/nvim-web-devicons" },
-    config = function()
-      require("nvim-tree").setup({
-        view = {
-          width = 30,
-          side = "left",
-        },
-        renderer = {
-          group_empty = true,
-          icons = {
-            show = {
-              git = true,
-            },
-          },
-        },
-        filters = {
-          dotfiles = false,
-          custom = { ".DS_Store" },
-        },
-        git = {
-          enable = true,
-          ignore = false,
-        },
-        actions = {
-          open_file = {
-            quit_on_open = false,
-          },
-        },
-      })
+	-- File explorer
+	{
+		"nvim-tree/nvim-tree.lua",
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+		config = function()
+			require("nvim-tree").setup({
+				view = {
+					width = 30,
+					side = "left",
+				},
+				renderer = {
+					group_empty = true,
+					icons = {
+						show = {
+							git = true,
+						},
+					},
+				},
+				filters = {
+					dotfiles = false,
+					custom = { ".DS_Store" },
+				},
+				git = {
+					enable = true,
+					ignore = false,
+				},
+				actions = {
+					open_file = {
+						quit_on_open = false,
+					},
+				},
+			})
 
-      -- Keymaps
-      vim.keymap.set("n", "<leader>e", "<cmd>NvimTreeToggle<CR>", { desc = "Toggle file explorer" })
-      vim.keymap.set("n", "<leader>ef", "<cmd>NvimTreeFindFileToggle<CR>", { desc = "Toggle file explorer on current file" })
-    end,
-  },
+			-- Keymaps
+			vim.keymap.set("n", "<leader>e", "<cmd>NvimTreeToggle<CR>", { desc = "Toggle file explorer" })
+			vim.keymap.set(
+				"n",
+				"<leader>ef",
+				"<cmd>NvimTreeFindFileToggle<CR>",
+				{ desc = "Toggle file explorer on current file" }
+			)
+		end,
+	},
 
-  -- Fuzzy finder
-  {
-    "nvim-telescope/telescope.nvim",
-    branch = "0.1.x",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
-    },
-    config = function()
-      local telescope = require("telescope")
-      local actions = require("telescope.actions")
+	-- Fuzzy finder
+	{
+		"nvim-telescope/telescope.nvim",
+		branch = "0.1.x",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+		},
+		config = function()
+			local telescope = require("telescope")
+			local actions = require("telescope.actions")
 
-      telescope.setup({
-        defaults = {
-          path_display = { "truncate" },
-          mappings = {
-            i = {
-              ["<C-k>"] = actions.move_selection_previous,
-              ["<C-j>"] = actions.move_selection_next,
-              ["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
-            },
-          },
-        },
-      })
+			telescope.setup({
+				defaults = {
+					path_display = { "truncate" },
+					mappings = {
+						i = {
+							["<C-k>"] = actions.move_selection_previous,
+							["<C-j>"] = actions.move_selection_next,
+							["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
+						},
+					},
+				},
+			})
 
-      telescope.load_extension("fzf")
+			telescope.load_extension("fzf")
 
-      -- Keymaps
-      local keymap = vim.keymap
-      keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>", { desc = "Fuzzy find files in cwd" })
-      keymap.set("n", "<leader>fr", "<cmd>Telescope oldfiles<cr>", { desc = "Fuzzy find recent files" })
-      keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>", { desc = "Find string in cwd" })
-      keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>", { desc = "Find string under cursor in cwd" })
-      keymap.set("n", "<leader>fb", "<cmd>Telescope buffers<cr>", { desc = "Find buffers" })
-      keymap.set("n", "<leader>fh", "<cmd>Telescope help_tags<cr>", { desc = "Find help" })
-      keymap.set("n", "<leader>fk", "<cmd>Telescope keymaps<cr>", { desc = "Find keymaps" })
-    end,
-  },
+			-- Keymaps
+			local keymap = vim.keymap
+			keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>", { desc = "Fuzzy find files in cwd" })
+			keymap.set("n", "<leader>fr", "<cmd>Telescope oldfiles<cr>", { desc = "Fuzzy find recent files" })
+			keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>", { desc = "Find string in cwd" })
+			keymap.set(
+				"n",
+				"<leader>fc",
+				"<cmd>Telescope grep_string<cr>",
+				{ desc = "Find string under cursor in cwd" }
+			)
+			keymap.set("n", "<leader>fb", "<cmd>Telescope buffers<cr>", { desc = "Find buffers" })
+			keymap.set("n", "<leader>fh", "<cmd>Telescope help_tags<cr>", { desc = "Find help" })
+			keymap.set("n", "<leader>fk", "<cmd>Telescope keymaps<cr>", { desc = "Find keymaps" })
+		end,
+	},
 
-  -- Which-key (show keybindings)
-  {
-    "folke/which-key.nvim",
-    event = "VeryLazy",
-    init = function()
-      vim.o.timeout = true
-      vim.o.timeoutlen = 300
-    end,
-    config = function()
-      require("which-key").setup({})
-    end,
-  },
+	-- Which-key (show keybindings)
+	{
+		"folke/which-key.nvim",
+		event = "VeryLazy",
+		init = function()
+			vim.o.timeout = true
+			vim.o.timeoutlen = 300
+		end,
+		config = function()
+			require("which-key").setup({})
+		end,
+	},
 
-  -- Auto pairs
-  {
-    "windwp/nvim-autopairs",
-    event = "InsertEnter",
-    config = true,
-  },
+	-- Auto pairs
+	{
+		"windwp/nvim-autopairs",
+		event = "InsertEnter",
+		config = true,
+	},
 
-  -- Comment
-  {
-    "numToStr/Comment.nvim",
-    config = function()
-      require("Comment").setup({
-        toggler = {
-          line = "gcc",
-          block = "gbc",
-        },
-        opleader = {
-          line = "gc",
-          block = "gb",
-        },
-      })
-    end,
-  },
+	-- Comment
+	{
+		"numToStr/Comment.nvim",
+		config = function()
+			require("Comment").setup({
+				toggler = {
+					line = "gcc",
+					block = "gbc",
+				},
+				opleader = {
+					line = "gc",
+					block = "gb",
+				},
+			})
+		end,
+	},
 
-  -- Surround
-  {
-    "kylechui/nvim-surround",
-    version = "*",
-    event = "VeryLazy",
-    config = true,
-  },
+	-- Surround
+	{
+		"kylechui/nvim-surround",
+		version = "*",
+		event = "VeryLazy",
+		config = true,
+	},
 
-  -- Better text objects
-  {
-    "echasnovski/mini.ai",
-    version = false,
-    config = function()
-      require("mini.ai").setup({ n_lines = 500 })
-    end,
-  },
+	-- Better text objects
+	{
+		"echasnovski/mini.ai",
+		version = false,
+		config = function()
+			require("mini.ai").setup({ n_lines = 500 })
+		end,
+	},
 
-  -- Move between tmux and nvim
-  {
-    "christoomey/vim-tmux-navigator",
-    cmd = {
-      "TmuxNavigateLeft",
-      "TmuxNavigateDown",
-      "TmuxNavigateUp",
-      "TmuxNavigateRight",
-      "TmuxNavigatePrevious",
-    },
-    keys = {
-      { "<c-h>", "<cmd><C-U>TmuxNavigateLeft<cr>" },
-      { "<c-j>", "<cmd><C-U>TmuxNavigateDown<cr>" },
-      { "<c-k>", "<cmd><C-U>TmuxNavigateUp<cr>" },
-      { "<c-l>", "<cmd><C-U>TmuxNavigateRight<cr>" },
-      { "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
-    },
-  },
+	-- Move between tmux and nvim
+	{
+		"christoomey/vim-tmux-navigator",
+		cmd = {
+			"TmuxNavigateLeft",
+			"TmuxNavigateDown",
+			"TmuxNavigateUp",
+			"TmuxNavigateRight",
+			"TmuxNavigatePrevious",
+		},
+		keys = {
+			{ "<c-h>", "<cmd><C-U>TmuxNavigateLeft<cr>" },
+			{ "<c-j>", "<cmd><C-U>TmuxNavigateDown<cr>" },
+			{ "<c-k>", "<cmd><C-U>TmuxNavigateUp<cr>" },
+			{ "<c-l>", "<cmd><C-U>TmuxNavigateRight<cr>" },
+			{ "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
+		},
+	},
 
-  -- Todo comments
-  {
-    "folke/todo-comments.nvim",
-    dependencies = { "nvim-lua/plenary.nvim" },
-    config = function()
-      require("todo-comments").setup({
-        signs = true,
-        keywords = {
-          FIX = { icon = " ", color = "error", alt = { "FIXME", "BUG", "FIXIT", "ISSUE" } },
-          TODO = { icon = " ", color = "info" },
-          HACK = { icon = " ", color = "warning" },
-          WARN = { icon = " ", color = "warning", alt = { "WARNING", "XXX" } },
-          PERF = { icon = " ", alt = { "OPTIM", "PERFORMANCE", "OPTIMIZE" } },
-          NOTE = { icon = " ", color = "hint", alt = { "INFO" } },
-          TEST = { icon = "⏲ ", color = "test", alt = { "TESTING", "PASSED", "FAILED" } },
-        },
-      })
+	-- Todo comments
+	{
+		"folke/todo-comments.nvim",
+		dependencies = { "nvim-lua/plenary.nvim" },
+		config = function()
+			require("todo-comments").setup({
+				signs = true,
+				keywords = {
+					FIX = { icon = " ", color = "error", alt = { "FIXME", "BUG", "FIXIT", "ISSUE" } },
+					TODO = { icon = " ", color = "info" },
+					HACK = { icon = " ", color = "warning" },
+					WARN = { icon = " ", color = "warning", alt = { "WARNING", "XXX" } },
+					PERF = { icon = " ", alt = { "OPTIM", "PERFORMANCE", "OPTIMIZE" } },
+					NOTE = { icon = " ", color = "hint", alt = { "INFO" } },
+					TEST = { icon = "⏲ ", color = "test", alt = { "TESTING", "PASSED", "FAILED" } },
+				},
+			})
 
-      vim.keymap.set("n", "]t", function()
-        require("todo-comments").jump_next()
-      end, { desc = "Next todo comment" })
+			vim.keymap.set("n", "]t", function()
+				require("todo-comments").jump_next()
+			end, { desc = "Next todo comment" })
 
-      vim.keymap.set("n", "[t", function()
-        require("todo-comments").jump_prev()
-      end, { desc = "Previous todo comment" })
-    end,
-  },
+			vim.keymap.set("n", "[t", function()
+				require("todo-comments").jump_prev()
+			end, { desc = "Previous todo comment" })
+		end,
+	},
 
-  -- Smooth scrolling
-  {
-    "karb94/neoscroll.nvim",
-    config = function()
-      require("neoscroll").setup({
-        mappings = { "<C-u>", "<C-d>", "<C-b>", "<C-f>", "<C-y>", "<C-e>", "zt", "zz", "zb" },
-        hide_cursor = true,
-        stop_eof = true,
-        respect_scrolloff = false,
-        cursor_scrolls_alone = true,
-        easing_function = "quadratic",
-      })
-    end,
-  },
+	-- Smooth scrolling
+	{
+		"karb94/neoscroll.nvim",
+		config = function()
+			require("neoscroll").setup({
+				mappings = { "<C-u>", "<C-d>", "<C-b>", "<C-f>", "<C-y>", "<C-e>", "zt", "zz", "zb" },
+				hide_cursor = true,
+				stop_eof = true,
+				respect_scrolloff = false,
+				cursor_scrolls_alone = true,
+				easing_function = "quadratic",
+			})
+		end,
+	},
 }
+
--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -1,137 +1,139 @@
 -- UI Enhancement plugins
 
 return {
-  -- Status line
-  {
-    "nvim-lualine/lualine.nvim",
-    dependencies = { "nvim-tree/nvim-web-devicons" },
-    config = function()
-      require("lualine").setup({
-        options = {
-          theme = "gruvbox",
-          component_separators = { left = "", right = "" },
-          section_separators = { left = "", right = "" },
-        },
-        sections = {
-          lualine_a = { "mode" },
-          lualine_b = { "branch", "diff", "diagnostics" },
-          lualine_c = { "filename" },
-          lualine_x = { "encoding", "fileformat", "filetype" },
-          lualine_y = { "progress" },
-          lualine_z = { "location" },
-        },
-      })
-    end,
-  },
+	-- Status line
+	{
+		"nvim-lualine/lualine.nvim",
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+		config = function()
+			require("lualine").setup({
+				options = {
+					theme = "gruvbox",
+					component_separators = { left = "", right = "" },
+					section_separators = { left = "", right = "" },
+				},
+				sections = {
+					lualine_a = { "mode" },
+					lualine_b = { "branch", "diff", "diagnostics" },
+					lualine_c = { "filename" },
+					lualine_x = { "encoding", "fileformat", "filetype" },
+					lualine_y = { "progress" },
+					lualine_z = { "location" },
+				},
+			})
+		end,
+	},
 
-  -- Buffer line (tabs)
-  {
-    "akinsho/bufferline.nvim",
-    dependencies = "nvim-tree/nvim-web-devicons",
-    config = function()
-      require("bufferline").setup({
-        options = {
-          mode = "buffers",
-          separator_style = "slant",
-          always_show_bufferline = true,
-          show_buffer_close_icons = true,
-          show_close_icon = true,
-          color_icons = true,
-          diagnostics = "nvim_lsp",
-          diagnostics_indicator = function(count, level)
-            local icon = level:match("error") and " " or " "
-            return " " .. icon .. count
-          end,
-          offsets = {
-            {
-              filetype = "NvimTree",
-              text = "File Explorer",
-              highlight = "Directory",
-              separator = true,
-            },
-          },
-        },
-      })
-    end,
-  },
+	-- Buffer line (tabs)
+	{
+		"akinsho/bufferline.nvim",
+		dependencies = "nvim-tree/nvim-web-devicons",
+		config = function()
+			require("bufferline").setup({
+				options = {
+					mode = "buffers",
+					separator_style = "slant",
+					always_show_bufferline = true,
+					show_buffer_close_icons = true,
+					show_close_icon = true,
+					color_icons = true,
+					diagnostics = "nvim_lsp",
+					diagnostics_indicator = function(count, level)
+						local icon = level:match("error") and " " or " "
+						return " " .. icon .. count
+					end,
+					offsets = {
+						{
+							filetype = "NvimTree",
+							text = "File Explorer",
+							highlight = "Directory",
+							separator = true,
+						},
+					},
+				},
+			})
+		end,
+	},
 
-  -- Indent guides
-  {
-    "lukas-reineke/indent-blankline.nvim",
-    main = "ibl",
-    config = function()
-      require("ibl").setup({
-        indent = {
-          char = "│",
-        },
-        scope = {
-          enabled = true,
-          show_start = true,
-          show_end = false,
-        },
-      })
-    end,
-  },
+	-- Indent guides
+	{
+		"lukas-reineke/indent-blankline.nvim",
+		main = "ibl",
+		config = function()
+			require("ibl").setup({
+				indent = {
+					char = "│",
+				},
+				scope = {
+					enabled = true,
+					show_start = true,
+					show_end = false,
+				},
+			})
+		end,
+	},
 
-  -- Dashboard
-  {
-    "goolord/alpha-nvim",
-    event = "VimEnter",
-    dependencies = { "nvim-tree/nvim-web-devicons" },
-    config = function()
-      local alpha = require("alpha")
-      local dashboard = require("alpha.themes.dashboard")
+	-- Dashboard
+	{
+		"goolord/alpha-nvim",
+		event = "VimEnter",
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+		config = function()
+			local alpha = require("alpha")
+			local dashboard = require("alpha.themes.dashboard")
 
-      dashboard.section.header.val = {
-        "                                                     ",
-        "  ███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗ ",
-        "  ████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║ ",
-        "  ██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║ ",
-        "  ██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║ ",
-        "  ██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║ ",
-        "  ╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝ ",
-        "                                                     ",
-      }
+			dashboard.section.header.val = {
+				"                                                                ",
+				"   /$$$$$$$  /$$$$$$$$ /$$$$$$$$/$$    /$$ /$$$$$$ /$$      /$$ ",
+				"  | $$__  $$| $$_____/|_____ $$| $$   | $$|_  $$_/| $$$    /$$$ ",
+				"  | $$  \\ $$| $$           /$$/| $$   | $$  | $$  | $$$$  /$$$$ ",
+				"  | $$  | $$| $$$$$       /$$/ |  $$ / $$/  | $$  | $$ $$/$$ $$ ",
+				"  | $$  | $$| $$__/      /$$/   \\  $$ $$/   | $$  | $$  $$$| $$ ",
+				"  | $$  | $$| $$        /$$/     \\  $$$/    | $$  | $$\\  $ | $$",
+				"  | $$$$$$$/| $$$$$$$$ /$$$$$$$$  \\  $/    /$$$$$$| $$ \\/  | $$",
+				"  |_______/ |________/|________/   \\_/    |______/|__/     |__/",
+			}
 
-      dashboard.section.buttons.val = {
-        dashboard.button("f", "  Find file", ":Telescope find_files <CR>"),
-        dashboard.button("e", "  New file", ":ene <BAR> startinsert <CR>"),
-        dashboard.button("r", "  Recently used files", ":Telescope oldfiles <CR>"),
-        dashboard.button("t", "  Find text", ":Telescope live_grep <CR>"),
-        dashboard.button("c", "  Configuration", ":e ~/.config/nvim/init.lua <CR>"),
-        dashboard.button("q", "  Quit Neovim", ":qa<CR>"),
-      }
+			dashboard.section.buttons.val = {
+				dashboard.button("f", "  Find file", ":Telescope find_files <CR>"),
+				dashboard.button("e", "  New file", ":ene <BAR> startinsert <CR>"),
+				dashboard.button("r", "  Recently used files", ":Telescope oldfiles <CR>"),
+				dashboard.button("t", "  Find text", ":Telescope live_grep <CR>"),
+				dashboard.button("c", "  Configuration", ":e ~/.config/nvim/init.lua <CR>"),
+				dashboard.button("q", "  Quit Neovim", ":qa<CR>"),
+			}
 
-      dashboard.section.footer.val = "🚀 Happy Coding!"
+			dashboard.section.footer.val = "🚀 Happy Coding!"
 
-      dashboard.config.opts.noautocmd = true
+			dashboard.config.opts.noautocmd = true
 
-      alpha.setup(dashboard.config)
-    end,
-  },
+			alpha.setup(dashboard.config)
+		end,
+	},
 
-  -- Notifications
-  {
-    "rcarriga/nvim-notify",
-    config = function()
-      require("notify").setup({
-        background_colour = "#000000",
-        render = "compact",
-        stages = "fade",
-      })
-      vim.notify = require("notify")
-    end,
-  },
+	-- Notifications
+	{
+		"rcarriga/nvim-notify",
+		config = function()
+			require("notify").setup({
+				background_colour = "#000000",
+				render = "compact",
+				stages = "fade",
+			})
+			vim.notify = require("notify")
+		end,
+	},
 
-  -- Better UI for vim.ui.select and vim.ui.input
-  {
-    "stevearc/dressing.nvim",
-    event = "VeryLazy",
-    config = true,
-  },
+	-- Better UI for vim.ui.select and vim.ui.input
+	{
+		"stevearc/dressing.nvim",
+		event = "VeryLazy",
+		config = true,
+	},
 
-  -- Icon picker
-  {
-    "nvim-telescope/telescope-symbols.nvim",
-  },
+	-- Icon picker
+	{
+		"nvim-telescope/telescope-symbols.nvim",
+	},
 }
+
